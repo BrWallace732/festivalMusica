0a27a6bb2f9384c6d2db8770e7ec2b1e604814c1
bruno Wallace <bruwallaceag@gmail.com>
2021-04-01 13:33:55 -0600

    Primeros ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
-const { src, dest } = require('gulp')
+const { src, dest,series, parallel } = require('gulp')
 const sass = require('gulp-sass')
 
 sass.compiler = require('dart-sass');
 
 
-function css(){
+function css( done ){
     
     return src("./src/scss/app.scss")
     .pipe(sass())
     .pipe( dest("./build/css"));
 }
-
+ function javascript(done){
+     console.log('compilando Javascript...');
+     done();
+ }
 
 exports.css = css
+exports.javascript = javascript
+exports.default = parallel( css, javascript)
