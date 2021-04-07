d633741be5045512ba65ca36513cb52f9664bb42
bruno Wallace <bruwallaceag@gmail.com>
2021-04-06 19:14:17 -0500

    Primeros Avances

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest,series, parallel } = require('gulp')
+const { src, dest, series, parallel, watch } = require('gulp')
 const sass = require('gulp-sass')
 
 sass.compiler = require('dart-sass');
@@ -6,7 +6,7 @@ sass.compiler = require('dart-sass');
 
 function css( done ){
     
-    return src("./src/scss/app.scss")
+    return src("src/scss/app.scss")
     .pipe(sass())
     .pipe( dest("./build/css"));
 }
@@ -15,6 +15,21 @@ function css( done ){
      done();
  }
 
+ function minificarcss(){
+    return src("src/scss/app.scss")
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe( dest("./build/css"));
+ }
+
+ function watchArchivos(){
+     watch("src/scss/**/*.scss", css ) // * carpeta actual  ** = todos los archivos
+     
+ }
+
 exports.css = css
 exports.javascript = javascript
+exports.minificarcss = minificarcss
+exports.watchArchivos = watchArchivos
 exports.default = parallel( css, javascript)
