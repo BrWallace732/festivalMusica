7f31c5f14111338390a9145572a78eb1eda030e2
bruno Wallace <bruwallaceag@gmail.com>
2021-04-07 19:31:57 -0500

    VideoHeader y lineup

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,54 @@
 const { src, dest, series, parallel, watch } = require('gulp')
 const sass = require('gulp-sass')
+const imagemin = require('gulp-imagemin')
+const notify = require('gulp-notify')
+const webp = require('gulp-webp')
 
 sass.compiler = require('dart-sass');
 
+const paths={
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss' // * carpeta actual  ** = todos los archivos     
+}
 
-function css( done ){
+
+function css(){
     
-    return src("src/scss/app.scss")
-    .pipe(sass())
+    return src(paths.scss)
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }))
     .pipe( dest("./build/css"));
 }
- function javascript(done){
-     console.log('compilando Javascript...');
-     done();
- }
 
- function minificarcss(){
-    return src("src/scss/app.scss")
+function minificarcss(){
+    return src(paths.scss)
     .pipe(sass({
         outputStyle: 'compressed'
     }))
     .pipe( dest("./build/css"));
- }
+}
+function imagenes(){
+    return src(paths.imagenes)
+    .pipe( imagemin() )
+    .pipe(dest('./build/img'))
+    .pipe(notify({message: 'imagen minificada'}))
+}
+function versionwebp(){
+    return src(paths.imagenes)
+        .pipe( webp())
+        .pipe( dest('./build/img'))
+        .pipe(notify({message: 'version webp lista'}))
+}
 
- function watchArchivos(){
-     watch("src/scss/**/*.scss", css ) // * carpeta actual  ** = todos los archivos
-     
- }
+
+function watchArchivos(){
+     watch(paths.scss, css ) 
+}
 
 exports.css = css
-exports.javascript = javascript
 exports.minificarcss = minificarcss
+exports.imagenes = imagenes
 exports.watchArchivos = watchArchivos
-exports.default = parallel( css, javascript)
+
+exports.default = series(css, imagenes,versionwebp, watchArchivos)
