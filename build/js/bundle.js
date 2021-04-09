f78a4bd0ff36f6d94af5b20f0fd07a58d8f73f66
bruno Wallace <bruwallaceag@gmail.com>
2021-04-09 13:02:20 -0500

    finalizado v-1 (footer, tablas y barra)

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,4 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
+    navegacionFija();
+});
 
+function navegacionFija(){
+
+    const barra = document.querySelector('.header');
+
+
+    //registrar el intersection observer
+    const observer = new IntersectionObserver( function(entries){
+        console.log(entries[0]);
+        if(entries[0].isIntersecting){
+            barra.classList.remove('fijo');        
+        }else{
+            barra.classList.add('fijo');
+        }
+    });
+
+    
+    //elemento a observar
+    observer.observe(document.querySelector('.sobre-festival'));
+
+}
+
+
+function scrollNav(){
+    const  enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach(function(enlace){
+        enlace.addEventListener('click', function(e){
+            e.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value);
+            seccion.scrollIntoView({
+                behavior: 'smooth' 
+            });
+        });
+    });
+
+}
 document.addEventListener('DOMContentLoaded', function(){
     crearGaleria();
 });
